b6ef518d6b5f6a95c702ab29952c53e3408564f0
Yiheng <development.henryquan@gmail.com>
2018-04-20 20:42:36 +1000

    Adding project
    Another simple app

---
+++ savetext/src/app/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class App extends Comment {
+  render () {
+    return (
+      <View>
+        
+      </View>
+    )
+  }
+}
